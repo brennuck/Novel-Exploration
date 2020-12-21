c7aec84b60e326995d43a2ee2c861ac8b61917b4
Brennon Nuckols <brennuck@gmail.com>
2020-12-21 14:06:52 -0700

    set up state

--- novel-exploration/src/App.js
+++ novel-exploration/src/App.js
@@ -1,9 +1,39 @@
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React from 'react';
+
+import axios from 'axios';
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            input: "",
+            newBook: {
+                title: "",
+                author: "",
+                publisher: "",
+                published: "",
+                description: "",
+                category: "",
+                image: "",
+                infoLink: ""
+            }
+        }
+    }
+
+    handleChanges = e => {
+        this.setState({
+            ...this.state.input,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="appContainer">
+
+            </div>
+        )
+    }
 }
 
 export default App;
